39dc545f560da400598fd25371f04bca894358e3
Timoti Fregni <timotif@gmail.com>
2022-07-20 21:16:47 +0200

    switched version of direction_reduction

--- directions_reduction.c
+++ directions_reduction.c
@@ -3,71 +3,83 @@
 #include <stdlib.h>
 // arr: directions to reduce, sz: size of arr, lg size of returned array
 // function result will be freed
-char** dirReduc(char** arr, int sz, int* lg) {
-  // your code
-  *lg = 0;
-  // Initializing sums for the 2 axis
-  int n_s = 0;
-  int w_e = 0;
-  int circle = 0;
-  for (int i = 0; i < sz; i++)
-{
-  // printf("%d %s\n", i, arr[i]);
-  if (i < sz - 3 && strcmp(arr[i], "NORTH") == 0 && strcmp(arr[i + 1], "WEST") == 0 && strcmp(arr[i + 2], "SOUTH") == 0 && strcmp(arr[i + 3], "EAST") == 0)
-    circle += 1;
-    if (strcmp(arr[i], "NORTH") == 0)
-        n_s += 1;
-    if (strcmp(arr[i], "SOUTH") == 0)
-        n_s -= 1;
-    if (strcmp(arr[i], "WEST") == 0)
-        w_e -= 1;
-    if (strcmp(arr[i], "EAST") == 0)
-        w_e += 1;
-}
 
-*lg += (n_s < 0) ? -n_s : n_s;
-*lg += (w_e < 0) ? -w_e : w_e;
-for (int i = 0; i < circle; i++)
-    {*lg += 4;}
-printf("n_s = %d w_e = %d lg = %d circle = %d\n", n_s, w_e, *lg, circle);
-char **ans = malloc(sizeof(char*) * (*lg));
-int step;
-int i = 0;
-while (i < circle * 4)
+/* online solution: https://www.codewars.com/kata/reviews/5852cbecb1e9d3b3680000a0/groups/5a9f9556c87770e8e7002369 */
+
+char **dirReduc(char **arr, int sz, int *lg)
 {
-    printf("circle = %d i = %d\n", circle, i);
-    ans[i++] = "NORTH";
-    ans[i++] = "WEST";
-    ans[i++] = "SOUTH";
-    ans[i++] = "EAST";
-}
-step = (n_s < 0) ? 1 : -1;
-for (n_s = n_s; n_s != 0; n_s += step)
-{
-    printf("i = %d\n", i);
-    // ans[i] = malloc(sizeof(char) * 6);
-    printf("%s\n", (step == -1) ? "NORTH" : "SOUTH");
-    ans[i] = (step == -1) ? "NORTH" : "SOUTH";
-    i++;
-}
-step = (w_e < 0) ? 1 : -1;
-for (w_e = w_e; w_e != 0; w_e += step)
-{
-    printf("i = %d\n", i);
-    // ans[i] = malloc(sizeof(char) * 5);
-    printf("%s\n", (step == -1) ? "EAST" : "WEST");
-    ans[i] = (step == -1) ? "EAST" : "WEST";
-    i++;
-}
-for (int i = 0; i < *lg; i++)
-    printf("%s\t", ans[i]);
-printf("\n");
-  return ans;
+    // your code
+    char **tmp = (char **)malloc(sizeof(char *) * sz);
+    for (int i = 0; i < sz; i++)
+    {
+        // tmp[i] = malloc(sizeof(char) * 6);
+        tmp[i] = "\0";
+    }
+    *lg = 0;
+    int j = 0;
+    int i = 0;
+    for (int i = 0; i < sz; i++)
+    {
+        while (j > 0 && !*tmp[j])
+        {
+            printf("rewind ");
+            j--;
+        }
+        printf("j: %d\n", j);
+        printf("\nbegin i = %d j = %d lg = %d\n", i, j, *lg);
+        if ((strcmp(arr[i], "NORTH") == 0 && strcmp(tmp[j], "SOUTH") == 0) ||
+            (strcmp(arr[i], "SOUTH") == 0 && strcmp(tmp[j], "NORTH") == 0) ||
+            (strcmp(arr[i], "EAST") == 0 && strcmp(tmp[j], "WEST") == 0) ||
+            (strcmp(arr[i], "WEST") == 0 && strcmp(tmp[j], "EAST") == 0))
+        {
+            printf("opposite_dir %s %s i = %d j = %d\n", arr[i], tmp[j], i, j);
+            tmp[j] = "";
+            if (*lg > 0)
+                *lg -= 1;
+            continue;
+        }
+        if (*tmp[j])
+        {
+            printf("the str %s exists\n", tmp[j]);
+            j++;
+            printf("Allocating memory for tmp[%d]\n", j);
+            tmp[j] = malloc(sizeof(char) * 6);
+            *lg += 1;
+            strcpy(tmp[j], arr[i]);
+        }
+        else
+        {
+            printf("the str doesn't exists\n");
+            printf("Allocating memory for tmp[%d]\n", j);
+            tmp[j] = malloc(sizeof(char) * 6);
+            printf("overwriting %s\n", tmp[j]);
+            *lg += 1;
+            strcpy(tmp[j], arr[i]);
+        }
+        printf("%s %s\n", arr[i], tmp[j]);
+        printf("end i = %d j = %d lg = %d\n", i, j, *lg);
+    }
+    printf("i = %d j = %d\n", i, j);
+    printf("*lg = %d\n", *lg);
+    char **ans = (char**)malloc(sizeof(char*) * (*lg));
+    for (int i = 0; i < *lg; i++)
+    {
+        ans[i] = malloc(sizeof(char) * 6);
+        strcpy(ans[i], tmp[i]);
+        free(tmp[i]);
+    }
+    free(tmp);
+    for (int i = 0; i < *lg; i++)
+        printf("%s ", ans[i]);
+    printf("\n");
+    return (ans);
 }
 
 int main(void)
 {
-    char *arr[] = {"NORTH", "WEST", "SOUTH", "EAST", "NORTH", "WEST", "SOUTH", "EAST", "SOUTH", "NORTH"};
-    int lg = 1;
-    dirReduc(arr, 10, &lg);
+    char *arr[] = {"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"};
+    //char *arr[] = {"NORTH", "WEST", "SOUTH", "EAST", "NORTH", "WEST", "SOUTH", "EAST", "SOUTH", "NORTH"};
+    //char *arr[] = {"NORTH", "WEST", "SOUTH", "EAST"};
+    int lg;
+    dirReduc(arr, 7, &lg);
 }
